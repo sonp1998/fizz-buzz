70f620afa9cefc15ce7f4d83172cfec85618f8a1
sonp1998 <sonp1998@gmail.com>
2022-09-07 16:02:27 -0400

    Initial commit

---
+++ src/main/java/com/tlglearning/Main.java
@@ -0,0 +1,10 @@
+package com.tlglearning;
+
+public class Main {
+
+  public static void main(String[] args) {
+    for (int i = 01; i <= 100; i++) {
+      System.out.println(FizzBuzz.valueOf(i));
+    }
+  }
+}
