d900a10c197abda74a3622b7340fc5a61bbbf24f
sonp1998 <sonp1998@gmail.com>
2022-09-20 14:03:15 -0400

    added assert test .

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,14 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
   }
+
 }
